e320b26b800e3d8bae32499588a20ead45f62adf
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2017-12-30 13:46:54 -0700

    pulled in images, about 2 hours into tutorial

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿console.log("hello guy!");
+
+var theForm = document.getElementById("theForm");
+
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function() {
+    console.log("Buying Item!");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
