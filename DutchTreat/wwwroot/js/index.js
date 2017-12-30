45bb1030a786662605b6374bb0794cb7518ccff2
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2017-12-30 16:49:35 -0700

    on module five, adding more views

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,13 +1,27 @@
-﻿console.log("hello guy!");
+﻿$(document).ready(function () {
+    console.log("hello guy!");
 
-var theForm = document.getElementById("theForm");
 
-theForm.hidden = true;
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function() {
-    console.log("Buying Item!");
+    var theForm = $("#theForm");
+
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function() {
+        console.log("Buying Item!");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("you clicked oin " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form")
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
 });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
